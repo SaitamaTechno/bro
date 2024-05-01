8b0234d65a9edc39873ebd88f5784b823a08ecf5
SaitamaTechno <95487121+SaitamaTechno@users.noreply.github.com>
2024-05-01 11:42:29 +0300

    Update scripts2.js

--- scripts1.js
+++ scripts1.js
@@ -26,8 +26,8 @@ function read_file(file){
     //const xArray = [50,60,70,80,90,100,110,120,130,140,150];
     //const yArray = [7,8,8,9,9,9,10,11,14,14,15];
     //console.log(js)
-    const dls=read_file("PDI.json")
-    const dly=read_file("PDO.json")
+    const dls=read_file("PDO.json")
+    const dly=read_file("PDI.json")
     const fark_list=[]
     for(var i=0; i<dls.close.length; i++){
       fark=dly.close[i]-dls.close[i]
@@ -47,7 +47,7 @@ function read_file(file){
     // Define Layout
     const layout = {
       xaxis: {range: mylen, title: "Dates"},
-      yaxis: {range: mylen, title: "PDO - PDI"},  
+      yaxis: {range: mylen, title: "PDI - PDO"},  
       title: "PDI - PDO Fark Grafiği",
       paper_bgcolor: "lightblue", //background color of the chart container space
       plot_bgcolor: "lightyellow", //background color of plot area
